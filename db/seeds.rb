a1af195b5e6e0f50450b8527706fdc31a301315b
kcstarless <155853062+kcstarless@users.noreply.github.com>
2024-06-17 12:15:29 +1000

    added seeding for user and post

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,43 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+# Destroy all data
+Post.destroy_all
+User.destroy_all
+
+
+# Create User data
+User.create(username: 'tom', email: 'tom@example.com', password: '121212')
+User.create(username: 'peter', email: 'peter@example.com', password: '121212')
+User.create(username: 'dave', email: 'dave@example.com', password: '121212')
+
+p "created #{User.count} users"
+
+# Get all Users
+# Users = User.all
+
+# Create posts
+
+20.times do
+  Post.create(title: Faker::Locations::Australia.location,
+              body: Faker::Quotes,
+              user_id: User.all.sample.id)
+end
+
+p "created #{Post.count} posts"
+
+# Create flight data
+# Users.each_with_index do |departure_User, index|
+#   next_Use = Uses[(index + 1) % Uses.size] # Ensures arrival Use is different
+#   10.times do
+#     Flight.create(departure_User: departure_User,
+#                   arrival_User: next_Use,
+#                   flight_date_time: Faker::Time.between(from: DateTime.now, to: DateTime.now + 30),
+#                   flight_duration: Faker::Number.between(from: 60, to: 720))
+#   end
+# end
+
+# p "created #{Flight.count} flights"
