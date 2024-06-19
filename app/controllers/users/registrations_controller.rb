feb5e99f97a504037b274c6ba1452a9b8aeb13cd
kcstarless <155853062+kcstarless@users.noreply.github.com>
2024-06-19 19:17:21 +1000

    added: user registration controller for devise to permit :username and updated routes

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,16 @@
+# app/controllers/users/registrations_controller.rb
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
+
+  protected
+
+  # Permit the username parameter along with the default Devise parameters
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+end
